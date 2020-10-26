9396756fe8a808cd96b49ef39165c6f0db1c7fcd
Domingo Castillo <48081621+DomingoCast@users.noreply.github.com>
2020-10-26 12:33:20 +0100

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -5,5 +5,7 @@ using namespace std;
 int main()
 {
 	cout << "Si lees esto y te llamas Domingo te quedan 2 horas";
+	cout << "He llegado a tiempo???????????? LMAOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO";
+
 	return 0;
 }
